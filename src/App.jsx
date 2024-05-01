b6a517543fe43903352c1ad370fada55e684c7af
alexandru-posirca <axelltech.dev@gmail.com>
2024-05-02 00:00:05 +0300

    Display recipes data in UI in container recipe

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Recipes from "./pages/recipes/Recipes";
+import Recipes, { recipesLoader } from "./pages/recipes/Recipes";
 import Contact from "./pages/contact/Contact";
 import NotFound from "./pages/NotFound";
 import RootLayout from "./layouts/RootLayout";
+import RecipesLayout from "./layouts/RecipesLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="recipes" element={<Recipes />} />
       <Route path="contact" element={<Contact />} />
+      <Route path="recipes" element={<RecipesLayout />}>
+        <Route index element={<Recipes />} loader={recipesLoader} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Route>
   )
--- src/pages/recipes/Recipes.jsx
+++ src/pages/recipes/Recipes.jsx
@@ -8,6 +8,8 @@ function Recipes() {
   const recipes = useLoaderData();
   const [recipesFiltered, setRecipesFiltered] = useState(recipes);
 
+  console.log(recipesFiltered);
+
   return (
     <div>
       <ModuleTop
@@ -18,18 +20,26 @@ function Recipes() {
         <div className="flex justify-center p-2">
           <h2>Find all recipes</h2>
         </div>
-        <div>
-        <div className="recipe-item border border-solid w-[300px] rounded-xl p-6">
-          <div className="card h-full">
-            <Link className="card-link flex gap-4 flex-col">
-              <div>
-                <img src="" alt="recipe-img" />
+        <div className="flex flex-wrap justify-between gap-5">
+          {recipesFiltered.length === 0 ? (
+            <div className="text-2xl">
+              <p>No recipes in this category</p>
               </div>
-              <p>Time prepare: </p>
-              <p>Read recipes...</p>
-            </Link>
-          </div>
-        </div>
+          ) : (
+            recipesFiltered.map((recipe) => (
+            <div className="recipe-item border border-solid w-[300px] rounded-xl p-6" key={recipe.id}>
+              <div className="card h-full">
+                <Link className="card-link flex gap-4 flex-col">
+                  <div>
+                    <img src="" alt="recipe-img" />
+                  </div>
+                  <p>Time prepare: </p>
+                  <p>Read recipes...</p>
+                </Link>
+              </div>
+            </div>
+            ))
+          )}
         </div>
       </div>
     </div>
@@ -37,3 +47,17 @@ function Recipes() {
 }
 
 export default Recipes;
+
+
+export const recipesLoader = async () => {
+  try {
+    const res = await fetch("../data/db.json");
+    if (!res.ok) {
+      throw new Error("Error fetch data");
+    }
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err.message);
+  }
+ };
